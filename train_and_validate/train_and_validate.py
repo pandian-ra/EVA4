fcf6c0b173d79517d85de30f53b95d65ada58ec7
Pandian <5630870+pandian-raja@users.noreply.github.com>
2020-03-31 15:56:17 -0700

    Update train_and_validate.py

--- train_and_validate/train_and_validate.py
+++ train_and_validate/train_and_validate.py
@@ -7,9 +7,17 @@ import numpy as np
 class train_and_validate():
     def train(trainloader, device, model,EPOCH):
         criterion = nn.CrossEntropyLoss()
-        optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=0.0001)
+        optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=3, verbose=True)
+        train_losses = []
+        train_acc = []
+        # pbar = tqdm(trainloader)
         for epoch in range(EPOCH):
             running_loss = 0.0
+            correct = 0
+            total = 0
+            processed = 0
+            running_loss_overall = 0.0
             for i, data in enumerate(trainloader, 0):
                 # get the inputs
                 inputs, labels = data
@@ -20,16 +28,37 @@ class train_and_validate():
                 outputs = model(inputs)
                 loss = criterion(outputs, labels)
                 loss.backward()
-                optimizer.step()
+                optimizer.step()  
 
+                pred = outputs.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+                correct += pred.eq(labels.view_as(pred)).sum().item()
+                processed += len(data)
+
+                # pbar.set_description(desc= f'Loss={loss.item()} Batch_id={i} Accuracy={100*correct/processed:0.2f}')
+                # train_acc.append(100*correct/processed)
+                
                 # print statistics
                 running_loss += loss.item()
+
                 if i % 2000 == 1999:    # print every 2000 mini-batches
                     print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+                    running_loss_overall += running_loss
                     running_loss = 0.0
-
+            
+            # print('Epoch {} completed'.format(epoch))
+            # print('Loss: {}. Accuracy: {}'.format(loss.item(), accuracy))
+            # print('-'*20)
+            # accuracy = 100 * correct / total
+            print((running_loss_overall / (i + 1)))
+            scheduler.step(100-(running_loss_overall / (i + 1)))
+            train_acc.append(100*correct/processed)
+            train_losses.append((100-(running_loss_overall / (i + 1))))
+        lr_finder = LRFinder(model, optimizer, criterion, device="cuda")
+        lr_finder.range_test(trainloader, end_lr=100, num_iter=100)
+        lr_finder.plot() # to inspect the loss-learning rate graph
+        lr_finder.reset()
         print('Finished Training')
-        return model
+        return model, train_acc, train_losses
     
     def validate(testloader, device, model):
         pred_wrong = []
